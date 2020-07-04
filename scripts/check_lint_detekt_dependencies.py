734d4bacd467fdd9a8f0aea816aee34c5e046555
Daniele Bottillo <daniele.bottillo@deliveroo.co.uk>
2020-07-04 16:51:36 +0100

    detekt and ktlint check script

---
+++ scripts/check_lint_detekt_dependencies.py
@@ -0,0 +1,27 @@
+# coding=utf-8
+import os
+import requests
+
+
+def check_detekt_version(currentversion):
+    r = requests.get('https://api.github.com/repos/detekt/detekt/releases/latest')
+    latest_version = r.json()['tag_name']
+    print("Detekt: " + currentversion + " -> " + latest_version)
+
+
+def check_ktlint_version(currentversion):
+    r = requests.get('https://api.github.com/repos/pinterest/ktlint/releases/latest')
+    latest_version = r.json()['tag_name']
+    print("Ktlint: " + currentversion + " -> " + latest_version)
+
+
+def check_detekt_or_ktlint(filename):
+    if filename.startswith('detekt'):
+        check_detekt_version(filename.split('_')[1])
+    elif filename.startswith('ktlint'):
+        check_ktlint_version(filename.split('_')[1])
+
+
+for root, dirnames, filenames in os.walk("binary"):
+    for filename in filenames:
+        check_detekt_or_ktlint(filename)
--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ./gradlew dependencyUpdates -Drevision=release -DoutputFormatter=json
-
+python3 scripts/check_lint_detekt_dependencies.py
