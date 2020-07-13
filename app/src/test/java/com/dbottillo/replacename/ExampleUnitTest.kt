443b592571a260eeed2dd34e2de7d9b5f9fbb918
Daniele Bottillo <daniele.bottillo@deliveroo.co.uk>
2020-07-13 16:57:37 +0100

    junit5 update

--- app/src/test/java/com/dbottillo/replacename/ExampleUnitTest.kt
+++ app/src/test/java/com/dbottillo/replacename/ExampleUnitTest.kt
@@ -1,16 +1,14 @@
 package com.dbottillo.replacename
 
-import org.junit.Assert.assertEquals
+import com.google.common.truth.Truth.assertThat
 import org.junit.Test
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class ExampleUnitTest {
+
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+        val result = 4
+
+        assertThat(result).isEqualTo(4)
     }
 }
